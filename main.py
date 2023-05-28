7796f18e9ab3ba863775d8f5200fb412dd13d73b
HeraticXYZ <54190606+HeraticXYZ@users.noreply.github.com>
2023-05-28 18:59:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,10 @@ from gedcom_functions import *
 
 gedcom = GEDCOM('Alvarez Family Tree 2022-04-14.ged')
 
-print(gedcom.FID_dict['F902']['MARR'])
-# known_grades = gedcom.get_known_grades('F902')
-# print(known_grades)
-# gedcom.pretty_print_known_grades(known_grades)
-# print('---')
-
 library = DispensationLibrary(gedcom)
-
 library.add_dispensa('F902', [(3, 3), (3, 4), (4, 4), (4, 4)])
+
+descendants = gedcom.get_relevant_descendants('F434')
+print('Relevant descendants of', gedcom.FID_dict['F434']['NAME'])
+print()
+gedcom.pretty_print_descendants(descendants)
